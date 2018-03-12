c795557aa1a87220214b8562c6294873cb8cb623
Renato Muniz <renato-muniz@hotmail.com>
2018-03-12 17:47:47 -0300

    Projeto criado

---
+++ src/main/java/com/sisdcon/AdminwebApplication.java
@@ -0,0 +1,12 @@
+package com.sisdcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminwebApplication.class, args);
+	}
+}
