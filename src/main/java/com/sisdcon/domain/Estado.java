21e4bd2b989e7f20a13b3145505477101bdbdeaa
Renato Muniz <renato-muniz@hotmail.com>
2018-03-20 13:05:04 -0300

    Create Estado.java

---
+++ src/main/java/com/sisdcon/domain/Estado.java
@@ -0,0 +1,110 @@
+package com.sisdcon.domain;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Estado implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="estado_id")
+	private Integer id;
+	
+	@Column(name="estado_nome")
+	private String nome;
+	
+	@Column(name="estado_sigla")
+	private String sigla;
+	
+	//-- 1 pra muitos
+	//@JsonManagedReference
+//	@JsonIgnore
+//	@OneToMany(mappedBy="cidade_estado")
+	//private List<Cidade> cidade = new ArrayList<>();
+	
+	@JsonIgnore
+	@OneToMany(mappedBy="estado")
+	private List<Cidade> cidades;
+
+	
+	public Estado() {
+	}
+
+	public Estado(Integer id, String nome, String sigla) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.sigla = sigla;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+
+	public List<Cidade> getCidades() {
+		return cidades;
+	}
+
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estado other = (Estado) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
